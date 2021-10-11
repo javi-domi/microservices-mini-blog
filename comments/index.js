907c5736ad07120a26d1521b9cf08444d09e73c7
javi-domi <javdominguez@icloud.com>
2021-10-11 15:34:56 -0300

    feat: implement posts and comments services

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { randomBytes } from "crypto";
+
+const app = express();
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(8081, () => {
+  console.log("app listening on port 8081");
+});
