9ef7e73c677a095c783410d410161f240a278f1c
javi-domi <javdominguez@icloud.com>
2021-10-19 22:36:18 -0300

    feat: implement Comment Moderation service

---
+++ moderation/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+
+app.post("/eventos", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("App listening on port 4003");
+});
