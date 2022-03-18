7e62c8f7725b14cf7f276130f00b0fe1617fb701
javi-domi <javdominguez@icloud.com>
2022-03-18 16:37:43 -0300

    feat: event bus data store

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
@@ -21,6 +23,10 @@ app.post("/events", (req, res) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
